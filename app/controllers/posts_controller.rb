c52813f87b4b063c52c502941c975f9e50e372fe
Hesham Salah <hesham@duot.io>
2016-07-25 14:10:46 +0200

    Modified > search, user authorizations, minor code styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,14 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    #This if statement is used to handle filtering by Tags, Useremail(Author), or Tilte
+    #This if statement is used to handle filtering by Tags, Useremail(Author),
+    #or Tilte
     if params[:tag] #this one is for tag filtering
       @posts = Post.tagged_with(params[:tag]).order("created_at DESC")
     elsif params[:search] #this one is for title search keywords
-      @posts = Post.where(title: params[:search].upcase)
-    elsif params[:user_email] #this one is for finding user email related to the post to retrieve all his posts
+      @posts = Post.where("title LIKE :t", {t: "%#{params[:search].upcase}%"})
+    elsif params[:user_email] #this one is for finding user email related to the
+                              # post to retrieve all his posts
       user_id = User.find_by(email: params[:user_email]).id
       @posts = Post.where(user_id: user_id)
     else
@@ -46,8 +48,14 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post.destroy
-    redirect_to root_path
+    user_id = current_user.id
+    if (current_user.id == @post.user_id)
+      @post.destroy
+      redirect_to root_path
+    else
+      flash[:alert] = "Your are not the Post Owner, You can't Delete it"
+      render 'show'
+    end
   end
 
   private
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,12 @@
 module PostsHelper
   def get_post_tags(post)
-    raw post.tag_list.map{ |t| link_to t, {controller: "posts", action: "index", tag: t} }.join(', ')
+    raw post.tag_list.map{ |t| link_to t, {controller: "posts", action: "index",
+                                           tag: t} }.join(', ')
   end
 
   def get_post_author(post)
-    link_to User.find(post.user_id).email, {controller: "posts", action: "index", user_email: User.find(post.user_id).email}
+    link_to User.find(post.user_id).email,
+                      {controller: "posts", action: "index",
+                       user_email: User.find(post.user_id).email}
   end
 end
